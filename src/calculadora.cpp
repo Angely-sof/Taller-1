0748ef96892c3e68f70e2b54c698a30dd54c6381
Angely-sof <bangely054@gmail.com>
2025-08-26 08:34:00 -0500

    Update calculadora.cpp

--- src/calculadora.cpp
+++ src/calculadora.cpp
@@ -1,1 +1,24 @@
-
+{
+    std:: string opt;
+    double x,y;
+    
+    std::cin >> x >>opt>> y;
+    
+    if(opt =="+"||opt == "suma"){
+        std::cout <<x+y<< std::endl;
+    }
+else if(opt =="-"||opt == "resta"){
+        std::cout <<x-y<< std::endl;
+    }
+        else if (opt =="*"||opt == "multiplicacion"){
+        std::cout <<x*y<< std::endl;
+    }
+     else if(opt =="/"||opt == "division"){
+    
+        if (y == 0){
+            std::cout << "error\n"<<std::endl;
+    		}
+    		else{ std::cout << (float) x/y << std::endl;}}
+    else{std::cout << "error\n" << std::endl;}
+    return 0;
+}
