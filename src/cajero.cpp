e93067f4b659826473a2614a1ebcb5730e8545a3
Angely-sof <bangely054@gmail.com>
2025-08-28 06:33:23 -0500

    Update cajero.cpp

--- src/cajero.cpp
+++ src/cajero.cpp
@@ -2,71 +2,64 @@
 using namespace std;
 
 int main() {
-	const int PIN = 1234;
-	int pin;
-	float saldo = 1000;
-	int opcion;
-	float retiro, deposito;
+    int pin;
+    int PIN = 1234; 
+    float saldo = 1000; 
+    int opcion;
+    float retiro;
+    float deposito;
 
-	cout << "Ingrese PIN: ";
-	cin >> pin;
+    cout << "Ingrese PIN: ";
+    cin >> pin;
 
-	if (pin == PIN) {
-		cout << "Acceso concedido.\n";
-	} else {
-		cout << "PIN incorrecto.\n";
-		cout << "Ingrese PIN: ";
-		cin >> pin;
+    if (pin != PIN) {
+        cout << "PIN incorrecto. Intente de nuevo: ";
+        cin >> pin;
 
-		if (pin == PIN) {
-			cout << "Acceso concedido.\n";
-		} else {
-			cout << "PIN incorrecto.\n";
-			cout << "Ingrese PIN: ";
-			cin >> pin;
+        if (pin != PIN) {
+            cout << "PIN incorrecto. Ultimo intento: ";
+            cin >> pin;
 
-			if (pin == PIN) {
-				cout << "Acceso concedido.\n";
-			} else {
-				cout << "bloqueado muchos intentos.\n";
-				return 0;
-			}
-		}
-	}
+            if (pin != PIN) {
+                cout << "Muchos intentos. Acceso bloqueado" << endl;
+                return 0; 
+            }
+        }
+   }
+    cout << "1. Consultar saldo" << endl;
+    cout << "2. Retirar dinero" << endl;
+    cout << "3. Depositar dinero" << endl;
+    cout << "Elija una opcion: ";
+    cin >> opcion;
 
-	cout << "1. Consultar saldo\n";
-	cout << "2. Retirar dinero\n";
-	cout << "3. Depositar dinero\n";
-	cout << "Opcion: ";
-	cin >> opcion;
+    if (opcion == 1) {
+        cout << "Su saldo actual es: $" << saldo << endl;
+    }
+    else if (opcion == 2) {
+        cout << "Ingrese monto a retirar: ";
+        cin >> retiro;
 
-	switch (opcion) {
-	case 1:
-		cout << "Saldo actual:" << saldo << endl;
-		break;
+        if (retiro > 0 && retiro <= saldo) {
+            saldo = saldo - retiro;
+            cout << "Retiro exitoso. Nuevo saldo: $" << saldo << endl;
+        } else {
+            cout << "monto invalido." << endl;
+        }
+    }
+    else if (opcion == 3) {
+        cout << "Ingrese monto a depositar: ";
+        cin >> deposito;
 
-	case 2:
-		cout << "Monto a retirar: ";
-		cin >> retiro;
-		if (retiro > 0 && retiro <= saldo) {
-			saldo -= retiro;
-			cout << "Retiro exitoso. Nuevo saldo: " <<saldo << endl;
-		} else {
-			cout << " monto invalido.\n";
-		}
-		break;
+        if (deposito > 0) {
+            saldo = saldo + deposito;
+            cout << "Deposito exitoso. Nuevo saldo: $" << saldo << endl;
+        } else {
+            cout << "monto invalido." << endl;
+        }
+    }
+    else {
+        cout << "Opcion no valida." << endl;
+    }
 
-	case 3:
-		cout << "Monto a depositar: ";
-		cin >> deposito;
-		if (deposito > 0) {
-			saldo += deposito;
-			cout << "Deposito exitoso. Nuevo saldo: $" << saldo << endl;
-		} else {
-			cout << "Error: monto invalido.\n";
-		}
-		break;
-
-	}
-	return 0;
+    return 0;
 }
